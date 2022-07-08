490d05a25c40473f132dd3fcbed73e93fe086d05
alexbashkatov6 <alexbashkatov6@gmail.com>
2022-07-08 16:30:49 +0300

    files operations

--- E_Exceptions/Program.cs
+++ E_Exceptions/Program.cs
@@ -6,6 +6,43 @@ namespace E_Exceptions
     {
         static void Main(string[] args)
         {
+            try  // Files operations !
+            {
+                File.Copy("test.txt", "test_copy.txt", overwrite: true);
+                // .Move - for rename
+                // .Delete
+                // .Exists
+                // .Replace - content from file to file
+
+                bool existsDir = Directory.Exists("tmp");
+                if (existsDir)
+                {
+                    var files = Directory.EnumerateFiles("file_1", "*.txt", SearchOption.AllDirectories);
+                }
+
+                Path.GetExtension("my_file.txt");
+                Path.Combine("sd", "asfd", "dfgf");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static void FilesOperations()
+        {
+            string[] allLines = File.ReadAllLines("test.txt");
+            string alltext = File.ReadAllText("test.txt");
+            IEnumerable<string> lines = File.ReadLines("test.txt");  // lazy
+
+            File.WriteAllText("test_2.txt", "My name is john");
+            File.WriteAllLines("test_3.txt", new string[] { "My name", "is john" });
+            File.WriteAllBytes("test_4.txt", new byte[] { 72, 101, 108, 108, 111 });
+
+            Console.WriteLine(alltext);
+
+            Console.ReadLine();
+
             Stream fs = new FileStream("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             try
             {
@@ -14,10 +51,10 @@ namespace E_Exceptions
 
                 if (fs.CanWrite)
                 {
-                    fs.Write(strInBytes, 0, strInBytes.Length); 
+                    fs.Write(strInBytes, 0, strInBytes.Length);
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.ToString()}");
             }
@@ -26,10 +63,28 @@ namespace E_Exceptions
                 fs.Close();
             }
 
-            using(Stream readingStream = new FileStream("test.txt", FileMode.Open, FileAccess.Read))
-            {
+            Console.WriteLine("Write success");
 
+            using (Stream readingStream = new FileStream("test.txt", FileMode.Open, FileAccess.Read))
+            {
+                byte[] temp = new byte[readingStream.Length];
+                int bytesToRead = (int)readingStream.Length;
+                int bytesRead = 0;
+                while (bytesToRead > 0)
+                {
+                    int n = readingStream.Read(temp, bytesRead, bytesToRead);
+                    if (n == 0)
+                        break;
+
+                    bytesRead += n;
+                    bytesToRead -= n;
+                }
+                string str = Encoding.ASCII.GetString(temp, 0, temp.Length);
+                Console.WriteLine(str);
             }
+
+            Console.WriteLine("Read success");
+
         }
 
         static void Exc()
