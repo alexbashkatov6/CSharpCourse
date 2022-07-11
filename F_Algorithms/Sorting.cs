e10b74db4e103f0cd75b78639c16bdc55e5e9c77
alexbashkatov6 <alexbashkatov6@gmail.com>
2022-07-11 13:53:07 +0300

    bubblesort - unittest

---
+++ F_Algorithms/Sorting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F_Algorithms
+{
+    public class Sorting
+    {
+        public static void BubbleSort(int[] array)
+        {
+            for (int partIndex = array.Length - 1; partIndex >= 1; partIndex--)
+            {
+                // partIndex is index of element, after 
+                for (int i = 0; i < partIndex; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(array, i, i + 1);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+                return;
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
