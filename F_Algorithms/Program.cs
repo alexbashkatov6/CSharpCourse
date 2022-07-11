44ef501db3a40c7cdd4a10fdc6f3f469819b3906
alexbashkatov6 <alexbashkatov6@gmail.com>
2022-07-11 17:43:47 +0300

    sorting algorithms

--- F_Algorithms/Program.cs
+++ F_Algorithms/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(IterativeFactorial(3));
+            Console.WriteLine(RecursiveFactorial(3));
+        }
+
+        private static int IterativeFactorial(int number)
+        {
+            if (number == 0)
+                return 1;
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        private static int RecursiveFactorial(int number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * RecursiveFactorial(number - 1);
+            }
         }
     }
 }
--- F_Algorithms/Sorting.cs
+++ F_Algorithms/Sorting.cs
@@ -8,11 +8,20 @@ namespace F_Algorithms
 {
     public class Sorting
     {
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+                return;
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
         public static void BubbleSort(int[] array)
         {
             for (int partIndex = array.Length - 1; partIndex >= 1; partIndex--)
             {
-                // partIndex is index of element, after 
+                // partIndex is index of element, wall is after it
                 for (int i = 0; i < partIndex; i++)
                 {
                     if (array[i] > array[i + 1])
@@ -23,13 +32,133 @@ namespace F_Algorithms
             }
         }
 
-        private static void Swap(int[] array, int i, int j)
+        public static void SelectionSort(int[] array)
         {
-            if (i == j)
-                return;
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+            for (int partIndex = array.Length - 1; partIndex >= 1; partIndex--)
+            {
+                int largestAt = 0;
+                for (int i = 1; i <= partIndex; i++)
+                {
+                    if (array[i] > array[largestAt])
+                    {
+                        largestAt = i;
+                    }
+                }
+                Swap(array, partIndex, largestAt);
+            }
+        }
+
+        public static void InsertionSort(int[] array)
+        {
+            for (int partIndex = 1; partIndex < array.Length; partIndex++)
+            {
+                int curUnsorted = array[partIndex];
+                int i = 0;
+                for (i = partIndex; i > 0 && array[i - 1] > curUnsorted; i--)
+                {
+                    array[i] = array[i - 1];
+                }
+                array[i] = curUnsorted;
+            }
+        }
+
+        public static void ShellSort(int[] array)
+        {
+            int gap = 1;
+            while (gap < array.Length / 3)
+            {
+                gap = 3 * gap + 1;
+            }
+            while (gap >= 1)
+            {
+                for (int i = gap; i < array.Length; i++)
+                {
+                    for (int j = i; j >= gap && array[j] < array[j - gap]; j -= gap)
+                    {
+                        Swap(array, j, j - gap);
+                    }
+                }
+                gap /= 3;
+            }
+        }
+
+        public static void MergeSort(int[] array)
+        {
+            int[] aux = new int[array.Length];
+            Sort(0, array.Length - 1);
+
+            void Sort(int low, int high)
+            {
+                if (high <= low)
+                    return;
+                int mid = (low + high) / 2;
+                Sort(low, mid);
+                Sort(mid + 1, high);
+                Merge(low, mid, high);
+            }
+
+            void Merge(int low, int mid, int high)
+            {
+                if (array[mid] <= array[mid + 1])
+                    return;
+
+                int i = low;
+                int j = mid + 1;
+
+                Array.Copy(array, low, aux, low, high - low + 1);
+
+                for (int k = low; k <= high; k++)
+                {
+                    if (i > mid) array[k] = aux[j++];
+                    else if (j > high) array[k] = aux[i++];
+                    else if (aux[j] < aux[i]) array[k] = aux[j++];
+                    else array[k] = aux[i++];
+                }
+            }
+
+
+        }
+
+        public static void QuickSort(int[] array)
+        {
+            Sort(0, array.Length - 1);
+
+            void Sort(int low, int high)
+            {
+                if (high <= low)
+                    return;
+                int j = Partition(low, high);
+                Sort(low, j-1);
+                Sort(j+1, high);
+            }
+
+            int Partition(int low, int high)
+            {
+                int i = low;
+                int j = high + 1;
+
+                int pivot = array[low];
+                while (true)
+                {
+                    while (array[++i] < pivot)
+                    {
+                        if (i == high)
+                            break;
+                    }
+                    while (pivot < array[--j])
+                    {
+                        if (j == low)
+                            break;
+                    }
+                    if (i >= j)
+                        break;
+
+                    Swap(array, i, j);
+                }
+
+                Swap(array, low, j);
+                return j;
+            }
         }
     }
 }
